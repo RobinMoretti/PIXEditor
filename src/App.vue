20fcf38e82ef6b8b2a907196cc5c3f7854c5b73c
Robin Moretti <robinhenrimoretti@gmail.com>
2021-07-30 12:49:47 +0900

    tart to integrate typescript

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<div id="app" v-on:mousemove="updateMousePosition">
+		<router-view/>
+	</div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useStore } from './store'
+
+export default defineComponent({
+  setup() {
+    const store = useStore()
+    store.state.canUpdateMou/* sePosit */ion // typed as number
+  },
+})
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+  @import "./assets/sass/app.scss";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    padding: 12px;
   }
-}
+
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,38 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from 'vue'
+import { createStore, useStore as baseUseStore, Store } from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+// define your typings for the store state
+export interface State {
+	// mousePosition: object,
+	canUpdateMousePosition: boolean
+}
+
+// define injection key
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+	state: {
+		canUpdateMousePosition: false,
+	},
+	mutations: {
+		// updateMousePostion: function(state, position){
+		// 	state.mousePosition = position;
+		// },
+		// toggleMouseTracker: function(state, value = null){
+		// 	if(value != null)
+		// 		state.updateMousePosBool = value;
+		// 	else
+		// 		state.updateMousePosBool = !state.updateMousePosBool;
+		// },
+	},
+	actions: {
+	},
+	modules: {
+	}
 })
+
+
+// define your own `useStore` composition function
+export function useStore () {
+	return baseUseStore(key)
+}
