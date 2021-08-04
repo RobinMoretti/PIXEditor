36106e86a9aa26d9756d0c47e4050e16302571a1
Robin Moretti <robinhenrimoretti@gmail.com>
2021-08-04 13:55:44 +0900

    build

---
+++ dist/precache-manifest.a729e20fa00df5f39db397d01d928c21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d61bd6b6f0a7239c642",
+    "url": "/css/app.79af12e7.css"
+  },
+  {
+    "revision": "c8041f987a7c34d176ad7014374a29cf",
+    "url": "/fonts/Rubik-VariableFont_wght.c8041f98.ttf"
+  },
+  {
+    "revision": "7a6e05c06d3c30e948e9ed6453aee846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a4316c7a155999db7d0",
+    "url": "/js/about.55199e99.js"
+  },
+  {
+    "revision": "4d61bd6b6f0a7239c642",
+    "url": "/js/app.3d77b964.js"
+  },
+  {
+    "revision": "3f73c9754d22d9dcddd5",
+    "url": "/js/chunk-vendors.d67ee2af.js"
+  },
+  {
+    "revision": "60f4b128370a267bcbf8ae5261e52f11",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
