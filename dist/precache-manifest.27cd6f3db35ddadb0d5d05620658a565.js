fc291e491e750bd8488ffd407ebf48584ba830ca
Robin Moretti <robinhenrimoretti@gmail.com>
2021-08-08 14:14:48 +0900

    again and export 0.7.0

---
+++ dist/precache-manifest.27cd6f3db35ddadb0d5d05620658a565.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6823a0a438967393e3a0",
+    "url": "css/app.ece78df1.css"
+  },
+  {
+    "revision": "c8041f987a7c34d176ad7014374a29cf",
+    "url": "fonts/Rubik-VariableFont_wght.c8041f98.ttf"
+  },
+  {
+    "revision": "0f1e2b61aed03a535d2d3b2f80f8e7b5",
+    "url": "index.html"
+  },
+  {
+    "revision": "5fad87292eae9fd2fc2b",
+    "url": "js/about.f1e230e7.js"
+  },
+  {
+    "revision": "6823a0a438967393e3a0",
+    "url": "js/app.4a26b154.js"
+  },
+  {
+    "revision": "7f50004ed8de6a4c6d28",
+    "url": "js/chunk-vendors.66400a12.js"
+  },
+  {
+    "revision": "60f4b128370a267bcbf8ae5261e52f11",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "54d9fec979abfcfec6e9bd02d666a547",
+    "url": "pix-grid/pix-editor.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
