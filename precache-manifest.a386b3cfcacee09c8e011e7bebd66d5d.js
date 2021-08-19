608dbafe0065e04e095c90197d9faac1c216afb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-19 02:55:44 +0000

    Deploy RobinMoretti/PIXEditor to github.com/RobinMoretti/PIXEditor.git:gh-pages

---
+++ precache-manifest.a386b3cfcacee09c8e011e7bebd66d5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f5a63e4959b1522878",
+    "url": "css/app.a1f05e21.css"
+  },
+  {
+    "revision": "d6178eda82f78ffad49d8e88dc74fbde",
+    "url": "fonts/Brygada1918-VariableFont_wght.d6178eda.ttf"
+  },
+  {
+    "revision": "ddc1c416815910393d4e739f35741728",
+    "url": "fonts/Dokdo-Regular.ddc1c416.ttf"
+  },
+  {
+    "revision": "478a2e1d643459a5e0500265f80f8ffe",
+    "url": "fonts/Heebo-VariableFont_wght.478a2e1d.ttf"
+  },
+  {
+    "revision": "c8041f987a7c34d176ad7014374a29cf",
+    "url": "fonts/Rubik-VariableFont_wght.c8041f98.ttf"
+  },
+  {
+    "revision": "f5d491298d43411bd41102663ab5fc42",
+    "url": "index.html"
+  },
+  {
+    "revision": "6c3dc438ffdad012578d",
+    "url": "js/about.cf66244d.js"
+  },
+  {
+    "revision": "27f5a63e4959b1522878",
+    "url": "js/app.078cc245.js"
+  },
+  {
+    "revision": "204bb77785c8c67f41c5",
+    "url": "js/chunk-vendors.8e01bf0e.js"
+  },
+  {
+    "revision": "8b7de1b99cbf0d4c0d7bc69f923fca86",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "54d9fec979abfcfec6e9bd02d666a547",
+    "url": "pix-grid/pix-editor.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
